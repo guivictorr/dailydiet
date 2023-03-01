c8a80d156c5d749e6a1be94f401f274423d2f217
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-28 22:55:41 -0300

    adding custom colors to theme

---
+++ src/style/theme.ts
@@ -0,0 +1,22 @@
+import { extendTheme } from 'native-base'
+
+export const theme = extendTheme({
+  colors: {
+    redDark: '#BF3B44',
+    redMid: '#F3BABD',
+    redLight: '#F4E6E7',
+    greenDark: '#639339',
+    greenMid: '#CBE4B4',
+    greenLight: '#E5F0DB',
+
+    gray: {
+      100: '#1B1D1E',
+      200: '#333638',
+      300: '#5c6265',
+      400: '#b9bbbc',
+      500: '#dddedf',
+      600: '#eff0f0',
+      700: '#fafafa',
+    },
+  },
+})
