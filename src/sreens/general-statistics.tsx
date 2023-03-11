5cb37495851102b28e5a9bc18fd1952aaa796fbf
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-03-10 23:33:41 -0300

    adding resume to general statistic

--- src/sreens/general-statistics.tsx
+++ src/sreens/general-statistics.tsx
@@ -1,9 +1,36 @@
-import { Center, Text } from 'native-base'
+import { useNavigation } from '@react-navigation/native'
+import {
+  Center,
+  Heading,
+  Icon,
+  IconButton,
+  Text,
+  useTheme,
+  VStack,
+} from 'native-base'
+import { ArrowLeft } from 'phosphor-react-native'
+import { StackNavigationProp } from '../routes/app.routes'
 
 export function GeneralStatistics() {
+  const { colors } = useTheme()
+  const navigation = useNavigation<StackNavigationProp>()
   return (
-    <Center flex={1}>
-      <Text>Statistics</Text>
-    </Center>
+    <VStack>
+      <Center px="8" h={160} bg="greenLight" position="relative">
+        <IconButton
+          onPress={navigation.goBack}
+          position="absolute"
+          top="4"
+          left="24px"
+          icon={<Icon as={() => <ArrowLeft color={colors.greenDark} />} />}
+          _pressed={{
+            backgroundColor: 'greenMid',
+            rounded: 'full',
+          }}
+        />
+        <Heading fontSize="3xl">90,86%</Heading>
+        <Text fontSize="md">das refeições dentro da dieta</Text>
+      </Center>
+    </VStack>
   )
 }
