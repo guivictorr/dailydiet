6397836cf38533d37b33f0a17deaf21a18690c22
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-03-20 21:22:24 -0300

    feat: finishing the new meal form

--- src/sreens/new-meal.tsx
+++ src/sreens/new-meal.tsx
@@ -1,16 +1,51 @@
 import { useNavigation } from '@react-navigation/native'
 import {
+  Box,
+  Button,
   Center,
   Heading,
   HStack,
   Icon,
   IconButton,
+  ScrollView,
+  Text,
   useTheme,
   VStack,
 } from 'native-base'
 import { ArrowLeft } from 'phosphor-react-native'
+import { ReactNode } from 'react'
 import { Input } from '../components/input'
 import { StackNavigationProp } from '../routes/app.routes'
+
+type RadioButtonProps = {
+  children: ReactNode
+  color: 'red' | 'green'
+}
+
+function RadioButton({ children, color }: RadioButtonProps) {
+  return (
+    <Button
+      flex={1}
+      flexDir="row"
+      bg="gray.600"
+      rounded="6"
+      borderWidth={1}
+      borderColor="gray.600"
+      _pressed={{
+        bg: `${color}Light`,
+        borderWidth: 1,
+        borderColor: `${color}Dark`,
+      }}
+    >
+      <HStack alignItems="center">
+        <Box boxSize="2" mr="2" bg={`${color}Dark`} rounded="full" />
+        <Text fontSize="md" fontWeight="bold" color="gray.100">
+          {children}
+        </Text>
+      </HStack>
+    </Button>
+  )
+}
 
 export function NewMeal() {
   const { colors } = useTheme()
@@ -31,38 +66,43 @@ export function NewMeal() {
         />
         <Heading fontSize="lg">Nova refeição</Heading>
       </Center>
-      <VStack
-        space="8"
-        pt="10"
-        px="8"
-        bg="gray.700"
-        roundedTop="20"
-        mt={-2}
-        h="full"
-      >
-        <Input label="Nome" />
-        <Input
-          label="Descrição"
-          input={{
-            multiline: true,
-            h: 120,
-          }}
-        />
-        <HStack space="5" justifyContent="space-between">
+      <ScrollView pt="10" px="8" bg="gray.700" roundedTop="20" mt={-2} h="full">
+        <VStack space="6">
+          <Input label="Nome" />
           <Input
-            label="Data"
-            formControl={{
-              flex: 1,
+            label="Descrição"
+            input={{
+              multiline: true,
+              h: 120,
             }}
           />
-          <Input
-            label="Hora"
-            formControl={{
-              flex: 1,
-            }}
-          />
-        </HStack>
-      </VStack>
+          <HStack space="5" justifyContent="space-between">
+            <Input
+              label="Data"
+              formControl={{
+                flex: 1,
+              }}
+            />
+            <Input
+              label="Hora"
+              formControl={{
+                flex: 1,
+              }}
+            />
+          </HStack>
+          <VStack>
+            <Text mb="2" fontWeight="bold" fontSize="md">
+              Está dentro da dieta ?
+            </Text>
+            <HStack space="4" justifyContent="space-between">
+              <RadioButton color="green">Sim </RadioButton>
+              <RadioButton color="red">Não</RadioButton>
+            </HStack>
+          </VStack>
+
+          <Button>Cadastrar refeição</Button>
+        </VStack>
+      </ScrollView>
     </VStack>
   )
 }
