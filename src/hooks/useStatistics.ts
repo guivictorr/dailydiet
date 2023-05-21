2414987e014e08c954a3e68d4ed6d5819e409f26
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-21 00:18:04 -0300

    feat: adding streak statistic

--- src/hooks/useStatistics.ts
+++ src/hooks/useStatistics.ts
@@ -1,31 +1,44 @@
-import { useFocusEffect } from '@react-navigation/native'
-import { useCallback, useState } from 'react'
-import { getMeals, MealStorageDTO } from '../storage/meal'
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback, useState } from "react";
+import { getMeals, MealStorageDTO } from "../storage/meal";
 
 export function useStatistics() {
-  const [meals, setMeals] = useState<MealStorageDTO[]>([])
-  const totalMeals = meals.length
-  const totalMealsOnDiet = meals.filter(
-    (meal) => meal.isOnDiet === 'yes',
-  ).length
-  const totalMealsOffDiet = meals.filter(
-    (meal) => meal.isOnDiet === 'no',
-  ).length
+	const [meals, setMeals] = useState<MealStorageDTO[]>([]);
+	const totalMeals = meals.length;
+	const totalMealsOnDiet = meals.filter(
+		(meal) => meal.isOnDiet === "yes"
+	).length;
+	const totalMealsOffDiet = meals.filter(
+		(meal) => meal.isOnDiet === "no"
+	).length;
 
-  const percentageOnDiet = totalMeals
-    ? (totalMealsOnDiet * 100) / totalMeals
-    : 0
+	const percentageOnDiet = totalMeals
+		? (totalMealsOnDiet * 100) / totalMeals
+		: 0;
 
-  useFocusEffect(
-    useCallback(() => {
-      getMeals().then(setMeals)
-    }, []),
-  )
+	const onDietStreak = meals
+		.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+		.reduce((acc, meal) => {
+			if (meal.isOnDiet === "yes") {
+				acc++;
+			} else {
+				acc = 0;
+			}
 
-  return {
-    percentageOnDiet,
-    totalMealsOffDiet,
-    totalMealsOnDiet,
-    totalMeals,
-  }
+			return acc;
+		}, 0);
+
+	useFocusEffect(
+		useCallback(() => {
+			getMeals().then(setMeals);
+		}, [])
+	);
+
+	return {
+		percentageOnDiet,
+		totalMealsOffDiet,
+		totalMealsOnDiet,
+		totalMeals,
+		onDietStreak,
+	};
 }
--- src/sreens/general-statistics.tsx
+++ src/sreens/general-statistics.tsx
@@ -1,83 +1,92 @@
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from "@react-navigation/native";
 import {
-  Center,
-  Heading,
-  HStack,
-  ICenterProps,
-  Icon,
-  IconButton,
-  Stack,
-  Text,
-  useTheme,
-  VStack,
-} from 'native-base'
-import { ArrowLeft } from 'phosphor-react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { useStatistics } from '../hooks/useStatistics'
-import { StackNavigationProp } from '../routes/app.routes'
+	Center,
+	Heading,
+	HStack,
+	ICenterProps,
+	Icon,
+	IconButton,
+	Stack,
+	Text,
+	useTheme,
+	VStack,
+} from "native-base";
+import { ArrowLeft } from "phosphor-react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useStatistics } from "../hooks/useStatistics";
+import { StackNavigationProp } from "../routes/app.routes";
 
 type CenteredBoxProps = {
-  title: string
-  description: string
-} & ICenterProps
+	title: string;
+	description: string;
+} & ICenterProps;
 
 const CenteredBox = ({ title, description, ...rest }: CenteredBoxProps) => (
-  <Center bg="gray.600" rounded="8" p="4" {...rest}>
-    <Heading>{title}</Heading>
-    <Text textAlign="center">{description}</Text>
-  </Center>
-)
+	<Center bg="gray.600" rounded="8" p="4" {...rest}>
+		<Heading>{title}</Heading>
+		<Text textAlign="center">{description}</Text>
+	</Center>
+);
 
 export function GeneralStatistics() {
-  const { totalMeals, totalMealsOnDiet, totalMealsOffDiet, percentageOnDiet } =
-    useStatistics()
-  const insets = useSafeAreaInsets()
-  const { colors } = useTheme()
-  const navigation = useNavigation<StackNavigationProp>()
-  return (
-    <VStack pt={insets.top} bg="greenLight">
-      <Center px="8" h={160} bg="greenLight" position="relative">
-        <IconButton
-          onPress={navigation.goBack}
-          position="absolute"
-          top="4"
-          left="24px"
-          icon={<Icon as={() => <ArrowLeft color={colors.greenDark} />} />}
-          _pressed={{
-            backgroundColor: 'greenMid',
-            rounded: 'full',
-          }}
-        />
-        <Heading fontSize="3xl">{percentageOnDiet.toFixed(2)}%</Heading>
-        <Text fontSize="md">das refeições dentro da dieta</Text>
-      </Center>
-      <VStack bg="gray.700" roundedTop="20" mt={-2} h="full">
-        <Center>
-          <Heading fontSize="xs" mt="8" mb="6">
-            Estatísticas gerais
-          </Heading>
-          <Stack space="3" px="8" w="full">
-            <CenteredBox
-              title={totalMeals.toString()}
-              description="refeições registradas"
-            />
-            <HStack space="3" justifyContent="space-between">
-              <CenteredBox
-                title={totalMealsOnDiet.toString()}
-                description="refeições dentro da dieta"
-                flex={1}
-                bg="greenLight"
-              />
-              <CenteredBox
-                title={totalMealsOffDiet.toString()}
-                description="refeições fora da dieta"
-                flex={1}
-                bg="redLight"
-              />
-            </HStack>
-          </Stack>
-        </Center>
-      </VStack>
-    </VStack>
-  )
+	const {
+		totalMeals,
+		totalMealsOnDiet,
+		totalMealsOffDiet,
+		percentageOnDiet,
+		onDietStreak,
+	} = useStatistics();
+	const insets = useSafeAreaInsets();
+	const { colors } = useTheme();
+	const navigation = useNavigation<StackNavigationProp>();
+	return (
+		<VStack pt={insets.top} bg="greenLight">
+			<Center px="8" h={160} bg="greenLight" position="relative">
+				<IconButton
+					onPress={navigation.goBack}
+					position="absolute"
+					top="4"
+					left="24px"
+					icon={<Icon as={() => <ArrowLeft color={colors.greenDark} />} />}
+					_pressed={{
+						backgroundColor: "greenMid",
+						rounded: "full",
+					}}
+				/>
+				<Heading fontSize="3xl">{percentageOnDiet.toFixed(2)}%</Heading>
+				<Text fontSize="md">das refeições dentro da dieta</Text>
+			</Center>
+			<VStack bg="gray.700" roundedTop="20" mt={-2} h="full">
+				<Center>
+					<Heading fontSize="xs" mt="8" mb="6">
+						Estatísticas gerais
+					</Heading>
+					<Stack space="3" px="8" w="full">
+						<CenteredBox
+							title={totalMeals.toString()}
+							description="refeições registradas"
+						/>
+						<CenteredBox
+							title={onDietStreak.toString()}
+							description="maior sequência de refeições dentro da dieta"
+						/>
+						<HStack space="3" justifyContent="space-between">
+							<CenteredBox
+								title={totalMealsOnDiet.toString()}
+								description="refeições dentro da dieta"
+								flex={1}
+								bg="greenLight"
+							/>
+							<CenteredBox
+								title={totalMealsOffDiet.toString()}
+								description="refeições fora da dieta"
+								flex={1}
+								bg="redLight"
+							/>
+						</HStack>
+					</Stack>
+				</Center>
+			</VStack>
+		</VStack>
+	);
 }
