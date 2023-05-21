97c21d58c985a0fb1104d3187d2412e5128092db
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-21 00:23:58 -0300

    fix: fixing section list scroll on home screen

--- src/sreens/home.tsx
+++ src/sreens/home.tsx
@@ -1,4 +1,4 @@
-import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import {
   Box,
   Button,
@@ -11,39 +11,39 @@ import {
   SectionList,
   Text,
   VStack,
-} from 'native-base'
-import { Plus } from 'phosphor-react-native'
-import { useCallback, useState } from 'react'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import DailyDietLogo from '../../assets/logo.png'
-import { Meal } from '../components/meal'
-import { StatisticResume } from '../components/statistic-resume'
-import { UserPhoto } from '../components/user-photo'
-import { StackNavigationProp } from '../routes/app.routes'
-import { getMeals, MealStorageDTO } from '../storage/meal'
+} from "native-base";
+import { Plus } from "phosphor-react-native";
+import { useCallback, useState } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import DailyDietLogo from "../../assets/logo.png";
+import { Meal } from "../components/meal";
+import { StatisticResume } from "../components/statistic-resume";
+import { UserPhoto } from "../components/user-photo";
+import { StackNavigationProp } from "../routes/app.routes";
+import { getMeals, MealStorageDTO } from "../storage/meal";
 
 export function Home() {
   const [meals, setMeals] = useState<
     { title: string; data: MealStorageDTO[] }[]
-  >([])
-  const insets = useSafeAreaInsets()
-  const navigation = useNavigation<StackNavigationProp>()
+  >([]);
+  const insets = useSafeAreaInsets();
+  const navigation = useNavigation<StackNavigationProp>();
 
   useFocusEffect(
     useCallback(() => {
       getMeals().then((meals) => {
         const formattedMeals = meals.reduce((acc, meal) => {
-          const day = new Date(meal.date).getDay().toString()
-          acc[day] = acc[day] || { title: meal.date, data: [] }
-          acc[day].data.push(meal)
+          const day = new Date(meal.date).getDay().toString();
+          acc[day] = acc[day] || { title: meal.date, data: [] };
+          acc[day].data.push(meal);
 
-          return acc
-        }, {} as any)
+          return acc;
+        }, {} as any);
 
-        setMeals(Object.values(formattedMeals))
-      })
-    }, []),
-  )
+        setMeals(Object.values(formattedMeals));
+      });
+    }, [])
+  );
 
   return (
     <VStack flex={1} px="6" pt="5">
@@ -53,7 +53,7 @@ export function Home() {
         justifyContent="space-between"
       >
         <Image alt="Daily Diet" source={DailyDietLogo} />
-        <Pressable onPress={() => navigation.navigate('Profile')}>
+        <Pressable onPress={() => navigation.navigate("Profile")}>
           <UserPhoto />
         </Pressable>
       </HStack>
@@ -65,7 +65,7 @@ export function Home() {
         Refeições
       </Text>
       <Button
-        onPress={() => navigation.navigate('NewMeal', { mode: 'CREATE' })}
+        onPress={() => navigation.navigate("NewMeal", { mode: "CREATE" })}
         startIcon={<Icon as={<Plus color="white" size={18} />} />}
       >
         Nova Refeição
@@ -91,13 +91,14 @@ export function Home() {
         )}
         renderSectionHeader={({ section: { title } }) => (
           <Heading mt="8" fontSize="xl">
-            {new Date(title).toLocaleDateString('pt-br', {})}
+            {new Date(title).toLocaleDateString("pt-br", {})}
           </Heading>
         )}
         contentContainerStyle={{
-          flex: 1,
+          paddingBottom: 24,
+          flex: meals.length === 0 ? 1 : 0,
         }}
       />
     </VStack>
-  )
+  );
 }
